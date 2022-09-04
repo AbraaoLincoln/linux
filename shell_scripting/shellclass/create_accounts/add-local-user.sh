bc40df3b51c46a348e80f7b79199bc9feecc9335
Abraao Lincol <abraao.lincol@gmail.com>
2022-09-04 11:07:32 -0300

    organized

---
+++ shell_scripting/shellclass/create_accounts/add-local-user.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Create new user accounts
+
+if [[ "${UID}" -ne 0 ]]; then
+    echo "To execute this script you need to be root"
+    exit 1
+fi
+
+read -p "Enter username: " USERNAME
+
+read -p "Enter the name of who gonna be usaing this acount: " COMMENT
+
+read -p "Enter the initial password: " PASSWORD
+
+useradd -c "${COMMENT}" -m "${USERNAME}"
+
+if [[ "${?}" -ne 0 ]]; then
+    echo "The user account was not create"
+    exit 1
+fi
+
+echo "${PASSWORD}" | passwd --stdin "${USERNAME}"
+
+passwd -e "${USERNAME}"
+
+if [[ "${?}" -ne 0 ]]; then
+    echo "The password was not create"
+    exit 1
+fi
+
+echo "New user create succefully"
+
+echo "User name: ${USERNAME}"
+
+echo "Password: ${PASSWORD}"
+
+echo "Host: ${HOSTNAME}"
--- tools.md
+++ tools.md
@@ -5,7 +5,21 @@
 It’s a special file, used to write. Whatever is write to /dev/null will be discarded.
 
 ```
-
 id user &> /dev/null
+```
 
+## useradd
+
+This command can be used to create or update a user information
+
+```
+man useradd
+```
+
+## passwd
+
+This command can be used to create or update a user password
+
+```
+man passwd
 ```
